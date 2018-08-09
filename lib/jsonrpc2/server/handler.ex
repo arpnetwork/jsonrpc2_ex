8df64a32d302c4949fbdfbd71502256aea9c4c17
rdspot <rdspot@arpnetwork.org>
2018-08-09 16:52:02 +0800

    Add options to select or exclude specific functions

--- lib/jsonrpc2/server/handler.ex
+++ lib/jsonrpc2/server/handler.ex
@@ -3,6 +3,8 @@ defmodule JSONRPC2.Server.Handler do
 
   defmacro __using__(opts) do
     name = Keyword.get_lazy(opts, :name, fn -> Misc.module_name(__CALLER__.module) end)
+    only = Keyword.get(opts, :only)
+    except = Keyword.get(opts, :except)
 
     quote do
       import JSONRPC2.Server.Handler
@@ -10,12 +12,24 @@ defmodule JSONRPC2.Server.Handler do
       @before_compile JSONRPC2.Server.Handler
 
       @name unquote(name)
+      @only unquote(only)
+      @except unquote(except)
     end
   end
 
   defmacro __before_compile__(env) do
     mod = env.module
+
     functions = Module.definitions_in(mod) |> Keyword.keys()
+    only = Module.get_attribute(mod, :only) |> check_functions(functions)
+    except = Module.get_attribute(mod, :except) |> check_functions(functions)
+
+    functions =
+      cond do
+        not is_nil(only) -> only
+        not is_nil(except) -> functions -- except
+        true -> functions
+      end
 
     quote do
       def __name__, do: @name
@@ -32,5 +46,21 @@ defmodule JSONRPC2.Server.Handler do
     quote do
       unquote(name)
     end
+  end
+
+  defp check_functions(nil, _), do: nil
+
+  defp check_functions(list, functions) when is_list(list) do
+    assert(list == Enum.uniq(list))
+
+    Enum.each(list, fn fun ->
+      assert(is_atom(fun) && Enum.member?(functions, fun))
+    end)
+
+    list
+  end
+
+  defp assert(expression) do
+    unless expression, do: raise(CompileError)
   end
 end
